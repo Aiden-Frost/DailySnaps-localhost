ee106bca13efa87eb27188dd47d1788063f57dc5
adiver26 <44595860+adiver26@users.noreply.github.com>
2020-03-28 15:22:29 +0530

    Update views.py

--- website/news/views.py
+++ website/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 import requests
-from bs4 import BeautifulSoup as BSoup
+from newsapi import NewsApiClient
 from news.models import Headline
 
 # Create your views here.
